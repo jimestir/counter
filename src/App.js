51856c50052bb4c6caa4c99d540f9f732a5e8c70
jimmyvergara03 <jimmyestir@gmail.com>
2022-04-14 10:13:33 -0400

    Add counter with its increment, decrement and reset buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {value}
+        <button onClick={() => setValue(value + 1)}>+</button>
+        <button onClick={() => setValue(value - 1)}>-</button>
+        <button onClick={() => setValue(0)}>reset</button>
+      </div>
+      ;
+    </>
   );
 }
 
