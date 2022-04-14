20bdad43ceb72ce2ab714660bac270846d37f1d9
jimmyvergara03 <jimmyestir@gmail.com>
2022-04-14 10:39:22 -0400

    Add new counter

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import "./App.css";
 import useCounter from "./hooks/useCounter";
 
 function App() {
-  const { counter, increase, decrease, reset } = useCounter();
+  const counterA = useCounter();
+  const counterB = useCounter();
 
   return (
     <>
       <div className="App">
-        {counter}
-        <button onClick={increase}>+</button>
-        <button onClick={decrease}>-</button>
-        <button onClick={reset}>reset</button>
+        <section className="App__counterA">
+          {counterA.counter}
+          <button onClick={counterA.increase}>+</button>
+          <button onClick={counterA.decrease}>-</button>
+          <button onClick={counterA.reset}>reset</button>
+        </section>
+
+        <section className="App__counterB">
+          {counterB.counter}
+          <button onClick={counterB.increase}>+</button>
+          <button onClick={counterB.decrease}>-</button>
+          <button onClick={counterB.reset}>reset</button>
+        </section>
       </div>
       ;
     </>
